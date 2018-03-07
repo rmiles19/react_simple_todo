30b13798cc8f4307555f0f41e26a3778596bd0c3
Rachel Miles <milesrachel9@gmail.com>
2018-03-07 12:30:03 -0700

    more changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-
+import List from './List';
+import TodoForm from './TodoForm';
 
 class App extends Component {
   state = {
-    todos: [
-      {id: 1, name: 'Learn Rails', complete: true },
-      {id: 2, name: 'Learn React', complete: false}, 
-      {id: 3, name: 'Learn Redux', complete: false},
-    ],
+    todos: [],
+  }
+
+  byName = (x,y) => {
+    if (x.name > y.name)
+      return 1 
+    if (x.name < y.name)
+      return -1
+    return 0
+  }
+
+  getId = () => {
+    //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+   return Math.floor((1 + Math.random()) * 0x10000)
+     .toString(16)
+     .substring(1);
+  }
+
+  addTodo = (name) => {
+    const { todos } = this.state;
+    const todo = { name, id: this.getId(), complete: false }
+    this.setState({ todos: [todo, ...todos] })
   }
 
   render() {
     const { todos } = this.state;
     return (
-     <div>
-      <ul>
-        { todos.map( todo => {
-          return <li key={todo.id}>{todo.name}</li>
-        })}
-      </ul>
-     </div>
-    )
+      <div>
+        <TodoForm addItem={this.addTodo}/>
+        <List items={todos} />
+      </div>
+    );
   }
 }
 
